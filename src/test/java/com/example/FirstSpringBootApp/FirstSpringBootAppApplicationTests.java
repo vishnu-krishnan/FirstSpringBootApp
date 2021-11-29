4d80d032ac9f58790b3d3212f66b6fcc0c439ebc
vishnu-krishnan <vishnuoffcl@gmail.com>
2021-11-29 16:18:57 +0530

    first commit

---
+++ src/test/java/com/example/FirstSpringBootApp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
