07c979253344a18a71de224a2e48da3f0117052b
vishnu-krishnan <vishnuoffcl@gmail.com>
2021-11-29 17:36:56 +0530

    added my sql application properties

--- src/main/java/com/example/FirstSpringBootApp/FirstSpringBootAppApplication.java
+++ src/main/java/com/example/FirstSpringBootApp/FirstSpringBootAppApplication.java
@@ -2,22 +2,15 @@ package com.example.FirstSpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class FirstSpringBootAppApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(FirstSpringBootAppApplication.class, args);
 	}
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello","world");
-	}
+ 	
 
 }
--- src/main/java/com/example/FirstSpringBootApp/Student/StudentService.java
+++ src/main/java/com/example/FirstSpringBootApp/Student/StudentService.java
@@ -1,2 +1,16 @@
-package com.example.FirstSpringBootApp.Student;public class StudentService {
+package com.example.FirstSpringBootApp.Student;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(new Student(
+                "ABC1",
+                "Vishnu",
+                25,
+                "vishnu@abc.com"
+        ));
+    }
 }
