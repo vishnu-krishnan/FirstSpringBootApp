be658e298ea00d540e7a78386aef9ff7b2f34e3b
vishnu-krishnan <vishnuoffcl@gmail.com>
2021-11-29 16:34:26 +0530

    Restcontroller and first method

--- src/main/java/com/example/FirstSpringBootApp/FirstSpringBootAppApplication.java
+++ src/main/java/com/example/FirstSpringBootApp/FirstSpringBootAppApplication.java
@@ -2,12 +2,20 @@ package com.example.FirstSpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstSpringBootAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstSpringBootAppApplication.class, args);
+	}
+	@GetMapping
+	public String hello(){
+		return "Hello world";
 	}
 
 }
